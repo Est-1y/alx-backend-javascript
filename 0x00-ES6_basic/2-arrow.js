94c8b4aaa30d35cf1c66cf8ca51ee88e54d4b854
Est-1y <terryiana22@gmail.com>
2024-09-30 15:18:52 +0000

    arrow.js

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// The arrow functions.(default)
+
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
