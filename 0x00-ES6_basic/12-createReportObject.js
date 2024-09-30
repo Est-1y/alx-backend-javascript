90fbae5f2e5d283186edc566bab3c34907add8f6
Est-1y <terryiana22@gmail.com>
2024-09-30 15:55:22 +0000

    report objects

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,11 @@
+// Report objects
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
